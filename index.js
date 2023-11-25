d35a2e0eb9be9d9396b3b7d15d9c27f328a20467
joaocarrato <joaocarrato18@gmail.com>
2023-11-25 00:37:37 -0300

    #

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const produtos = require("./src/unity/unity.json");
+const unity = require("./src/unity/unity.json");
+const services = require("./src/services/services.json");
+const barber = require("./src/barber/barber.json");
+const schedule = require("./src/schedule/schedule.json");
 
 app.get("/unity", (req, res) => {
-  return res.json(produtos);
+  return res.json(unity);
+});
+
+app.get("/services", (req, res) => {
+  return res.json(services);
+});
+
+app.get("/barber", (req, res) => {
+  return res.json(barber);
+});
+
+app.get("/schedule", (req, res) => {
+  return res.json(schedule);
 });
 
 app.listen(port, () => {
